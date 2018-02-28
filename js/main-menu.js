7d96890ce8415c62c7f5def5ff1d8ff27db08df8
alexandr krug <smrnv42@gmail.com>
2018-02-28 12:20:22 +0300

    initital commit

---
+++ js/main-menu.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var pageheader = document.querySelector('.page-header');
+var menutoggle = document.querySelector('.toggle');
+var mainnav = document.querySelector('.main-nav');
+//var pagelogo = document.querySelector('.page-logo');
+var icon = document.querySelector('.toggle__icon');
+
+pageheader.classList.remove('page-header--nojs');
+
+//pagelogo.classList.remove('page-logo--open');
+mainnav.classList.remove('main-nav--opened');
+mainnav.classList.add('main-nav--closed');
+
+menutoggle.addEventListener('click', function () {
+  if (mainnav.classList.contains('main-nav--closed')) {
+    mainnav.classList.remove('main-nav--closed');
+    mainnav.classList.add('main-nav--opened');
+    icon.classList.add('toggle__icon--active');
+    //pagelogo.classList.add('page-logo--open');
+  } else {
+    mainnav.classList.remove('main-nav--opened');
+    mainnav.classList.add('main-nav--closed');
+    icon.classList.remove('toggle__icon--active');
+    //pagelogo.classList.remove('page-logo--open');
+  }
+});
