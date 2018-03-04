2ae89fb0397d3e7fb4ba478c082bbe6060c0b828
Sasha Krug <smrnv42@gmail.com>
2018-03-05 00:07:49 +0300

    Update 2018-03-04T21:07:38.093Z

--- js/header-slider.js
+++ js/header-slider.js
@@ -1,18 +1,1 @@
-"use strict";
-
-var container = document.querySelector(".page-header__slide-bg");
-var slides = document.querySelectorAll(".page-header__slide");
-
-var intervalID = setInterval(changeSlide, 3000);
-
-function changeSlide() {
-  var newSlideIndex = Number(container.dataset.currentSlide) % 2 + 1;
-
-  Array.prototype.forEach.call(slides, function(slide) {
-    slide.classList.remove("page-header__slide--shown");
-    if (Number(slide.dataset.slide) === newSlideIndex) {
-      container.dataset.currentSlide = newSlideIndex;
-      slide.classList.add("page-header__slide--shown");
-    }
-  });
-}
+"use strict";var container=document.querySelector(".page-header__slide-bg"),slides=document.querySelectorAll(".page-header__slide"),intervalID=setInterval(changeSlide,3e3);function changeSlide(){var e=Number(container.dataset.currentSlide)%2+1;Array.prototype.forEach.call(slides,function(a){a.classList.remove("page-header__slide--shown"),Number(a.dataset.slide)===e&&(container.dataset.currentSlide=e,a.classList.add("page-header__slide--shown"))})}
